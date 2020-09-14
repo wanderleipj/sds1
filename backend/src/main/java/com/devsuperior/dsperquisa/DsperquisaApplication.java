1861d48a69f7f012bf21e81275946888aa9d7004
Wanderlei Pinto Junior <wanderleipj@gmail.com>
2020-09-14 13:44:00 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsperquisa/DsperquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsperquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsperquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsperquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsperquisa/DsperquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsperquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsperquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
